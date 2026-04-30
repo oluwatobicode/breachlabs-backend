be8a6de39d014f155ed9447d210f60130ab62876
Coding Ninja <odetokuntreasure6@gmail.com>
2026-04-30 12:05:03 +0100

    cors-error

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,15 @@ app.use("/webhooks", webhookRoutes);
 
 // midddlware
 app.use(helmet());
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://breachlabs-app-frontend.vercel.app",
+    ],
+    credentials: true,
+  }),
+);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(clerkMiddleware());
