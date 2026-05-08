9b32369e75e1880f17e7d7d42fba6f9bb613f0f0
Coding Ninja <odetokuntreasure6@gmail.com>
2026-05-08 13:56:11 +0100

    feat:gate-paid-challenge-questions

--- src/controllers/challenge.controller.ts
+++ src/controllers/challenge.controller.ts
@@ -206,10 +206,6 @@ export const getChallengeById = async (
       return sendError(res, 404, "Challenge not found");
     }
 
-    if (!challenge.isFree && !hasActiveProAccess(req.user)) {
-      return sendError(res, 403, "This challenge requires a PRO subscription");
-    }
-
     const { deletedAt, ...rest } = challenge;
     return res.json({ challenge: rest });
   } catch (error) {
